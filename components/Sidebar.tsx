7cd932ea0041cf1cc26e89378da1e717e7caa072
Bmgaje <brijeshgajera4205@gmail.com>
2023-10-27 16:38:00 -0400

    add sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="p-2 flex-col h-screen ">
+      <div className="flex-1">
+        <div>
+          {/* new chat */}
+          <div>{/* modelSelection */}</div>
+          {/* map to chat rows */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
